897aec0c3c92ac9b5ca26f341f4bdc4e3193f20a
SystXD <technologyfullgyan32@gmail.com>
2025-03-30 07:34:52 +0530

    Prettier rc

--- XRouter/src/structure/hooks/xhooks.ts
+++ XRouter/src/structure/hooks/xhooks.ts
@@ -2,12 +2,19 @@ import { NextFunction, Request, Response } from "express";
 import { HooksOptions } from "../../typings";
 
 export abstract class XHooks {
-    public async preHook(_req: Request, _res: Response, next: NextFunction): Promise<void> {
-        next();
-    }
+  public async preHook(
+    _req: Request,
+    _res: Response,
+    next: NextFunction
+  ): Promise<void> {
+    next();
+  }
 
-    public async postHook(_req: Request, _res: Response, next: NextFunction): Promise<void> {
-        next();
-    }
-   
+  public async postHook(
+    _req: Request,
+    _res: Response,
+    next: NextFunction
+  ): Promise<void> {
+    next();
+  }
 }
--- XRouter/src/typings.ts
+++ XRouter/src/typings.ts
@@ -1,55 +1,54 @@
-import { Application, NextFunction, Request, Response } from "express"
+import { Application, NextFunction, Request, Response } from "express";
 
 export interface RouterOptions {
-    /**
-     * The path where all the routes are being created.
-     */
-    dir?: string    
-    /**
-     * Get access to Request & Response from XRouter Hooks.
-     * @example
-     * const { useRequest, useResponse } = require('xrouter')
-     * module.exports = () => { 
-     *    const [req, res] = [useRequest(), useResponse()]
-     *    res
-     *    .status(200)
-     *    .json({ success: true })
-     *  }
-     */
-    hooks?: boolean
-    /**
-     * The Base App Object.
-     * For more information, see {@link https://expressjs.com/en/api.html#app Express Application}.
-     */
-    app:Application
+  /**
+   * The path where all the routes are being created.
+   */
+  dir?: string;
+  /**
+   * Get access to Request & Response from XRouter Hooks.
+   * @example
+   * const { useRequest, useResponse } = require('xrouter')
+   * module.exports = () => {
+   *    const [req, res] = [useRequest(), useResponse()]
+   *    res
+   *    .status(200)
+   *    .json({ success: true })
+   *  }
+   */
+  hooks?: boolean;
+  /**
+   * The Base App Object.
+   * For more information, see {@link https://expressjs.com/en/api.html#app Express Application}.
+   */
+  app: Application;
 
-    /**
-     * The path for a single Route which accepts all HTTP Methods
-     */
-    catchAllRoutes?: string
+  /**
+   * The path for a single Route which accepts all HTTP Methods
+   */
+  catchAllRoutes?: string;
 }
-
 
 export interface MethodInfo {
-  method: string
-  path: string
+  method: string;
+  path: string;
 }
 export interface ContextOptions {
-    request:Request;
-    response:Response
-    next: NextFunction
+  request: Request;
+  response: Response;
+  next: NextFunction;
 }
 
 export interface FileInfo {
-   filePath: string
-   folderName: string
-   isFolder: boolean
+  filePath: string;
+  folderName: string;
+  isFolder: boolean;
 }
 
 export interface HooksOptions {
-   request: Request;
-   response: Response,
-   next: NextFunction
+  request: Request;
+  response: Response;
+  next: NextFunction;
 }
 
 export type HttpMethod = "get" | "post" | "put" | "patch" | "delete";
